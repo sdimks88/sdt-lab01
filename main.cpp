a5c82c8fe41aff46f36a49c2a331bbb88c4fc673
Дмитрий Струнников <strunnikov.sdimka@yandex.ru>
2017-03-09 15:53:10 +0300

    los

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ int main()
     int a,b;
   cout << "Enter a,b";
     cin >>a>>b;
-    cout << "a * 2= " << a + b << '\n';
+    cout << "a + b= " << a + b << '\n';
     cout <<"a * 2="<<a * 2 << '\n';
 }
